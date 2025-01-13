05980f6f870669a46a1c89eba251c083213dcc15
saadbhnas <saad.bhnas@gmail.com>
2025-01-13 21:31:04 +0300

    changing name version file to upercase VERSION

--- recommendation_model/__init__.py
+++ recommendation_model/__init__.py
@@ -1,5 +1,5 @@
 from recommendation_model.config.core import Package_root
 
-with open(Package_root / "VERSION") as version_file:
+with open(Package_root / "version") as version_file:
     __version__ = version_file.read().strip()
 
